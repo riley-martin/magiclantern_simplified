866d21f8c3b309459c803112a0759a771272b21e
reticulatedpines <33672591+reticulatedpines@users.noreply.github.com>
2022-04-16 18:20:58 +0100

    MMU parser: add CLI help, main(), some tidying of globals

--- contrib/firmware_tools/decode_armv7_mmu_tables.py
+++ contrib/firmware_tools/decode_armv7_mmu_tables.py
@@ -6,33 +6,100 @@
 
 import os
 import sys
+import argparse
 from struct import unpack
+
+ROM = None
+off = 0xE0000000
+
+def main():
+    args = parse_args()
+
+    global ROM
+    ROM = open(args.rom, "rb").read()
+
+    last_offset = None
+    vstart = 0
+    pstart = 0
+    size = 0
+    last_ap = None
+    last_xn = None
+    last_texcb = None
+
+    for a, p, s, ap, texcb, xn in scan([0xE0004800, 0xE0000000]):  # R5 core 0 (0xDFFC4800 / 0xDFFC0000, copied from 0xE0000000)
+    #~ for a, p, s, ap, texcb, xn in scan([0xE0004880, 0xE0000000]):  # R5 core 1 (0xDFFC4880 / 0xDFFC0000, copied from 0xE0000000)
+    #~ for a, p, s, ap, texcb, xn in scan([0xE0004800, 0xE0000080]):  # M50 core 0
+    #~ for a, p, s, ap, texcb, xn in scan([0xE0004880, 0xE0000080]):  # M50 core 1
+        offset = p - a
+        s = 1024
+        if offset == last_offset and ap == last_ap and xn == last_xn and texcb == last_texcb and a == vstart + size:
+            size += s
+        else:
+            if last_offset is not None:
+                print("%08X-%08X -> %08X-%08X (%+5X) %-4s %s %s" %
+                            (vstart, vstart+size-1, pstart, pstart+size-1,
+                            last_offset, decode_texcb(last_texcb), decode_ap(last_ap),
+                            decode_xn(last_xn)), file=sys.stderr)
+            vstart, pstart, size = a, p, s
+            last_offset = offset
+            last_ap = ap
+            last_xn = xn
+            last_texcb = texcb
+
+        #~ print "%08X-%08X: %08X-%08X (tmp)" % (vstart, vstart+size, pstart, pstart+size)
+
+    print("%08X-%08X -> %08X-%08X (%+5X) %-4s %s %s" % 
+                (vstart, vstart+size-1, pstart, pstart+size-1,
+                last_offset, decode_texcb(last_texcb), decode_ap(last_ap),
+                decode_xn(last_xn)), file=sys.stderr)
+
+
+def parse_args():
+    description = """
+    """
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("rom",
+                        help="path to ROM to attempt MMU table parsing")
+
+    args = parser.parse_args()
+    args.rom = os.path.realpath(args.rom)
+    if not os.path.isfile(args.rom):
+        print("ROM didn't exist: '%s'" % args.rom)
+        sys.exit(-1)
+
+    return args
+
 
 def getLongLE(d, address):
    return unpack('<L',d[address:address+4])[0]
 
+
 def getByte(d, address):
    return ord(d[address])
 
+
 def getString(d, address):
     return d[address : d.index('\0', address)]
+
 
 def extract32(value, start, length):
     assert(start >= 0 and length > 0 and length <= 32 - start);
     return (value >> start) & (0xFFFFFFFF >> (32 - length));
 
-ROM = open(sys.argv[1], "rb").read()
-off = 0xE0000000
 
 def scan(ttbrs):
     for i,ttbr in enumerate(ttbrs):
         print()
         print("TTBR%d: %08X" % (i, ttbr))
         print("===============")
-        if not ttbr: continue
+        if not ttbr:
+            continue
         for address in range(0, 1<<32, 1024):
             tbl = 1 if address >> (32-7) else 0
-            if tbl != i: continue
+            if tbl != i:
+                continue
 
             base_mask = 0xffffc000 if i == 1 else 0xffffff80
             entry_addr = (ttbr & base_mask) | ((address >> 18) & 0x3ffc)
@@ -100,13 +167,6 @@ def scan(ttbrs):
             #~ print >> sys.stderr, hex(address), hex(phys_addr)
             yield address, phys_addr, page_size, ap, texcb, xn
 
-last_offset = None
-vstart = 0
-pstart = 0
-size = 0
-last_ap = None
-last_xn = None
-last_texcb = None
 
 def decode_ap(ap):
     if (ap == 0b001):
@@ -117,6 +177,7 @@ def decode_ap(ap):
         return "RW  "
     print(bin(ap))
     assert False
+
 
 def decode_cached_attr(x):
     if x == 0b00:
@@ -132,6 +193,7 @@ def decode_cached_attr(x):
         #~ return "Write-Back, no Write-Allocate"
         return "WB,WN"
 
+
 def decode_texcb(texcb):
     if texcb  & 0b10000:
         return "O:%s I:%s " % (decode_cached_attr((texcb >> 2) & 3), decode_cached_attr(texcb & 3))
@@ -142,29 +204,12 @@ def decode_texcb(texcb):
     print(bin(texcb))
     assert False
 
+
 def decode_xn(xn):
     if xn:
         return "XN"
     return "  "
 
-for a, p, s, ap, texcb, xn in scan([0xE0004800, 0xE0000000]):  # R5 core 0 (0xDFFC4800 / 0xDFFC0000, copied from 0xE0000000)
-#~ for a, p, s, ap, texcb, xn in scan([0xE0004880, 0xE0000000]):  # R5 core 1 (0xDFFC4880 / 0xDFFC0000, copied from 0xE0000000)
-#~ for a, p, s, ap, texcb, xn in scan([0xE0004800, 0xE0000080]):  # M50 core 0
-#~ for a, p, s, ap, texcb, xn in scan([0xE0004880, 0xE0000080]):  # M50 core 1
-    offset = p - a
-    s = 1024
-    if offset == last_offset and ap == last_ap and xn == last_xn and texcb == last_texcb and a == vstart + size:
-        size += s
-    else:
-        if last_offset is not None:
-            print("%08X-%08X -> %08X-%08X (%+5X) %-4s %s %s" % (vstart, vstart+size-1, pstart, pstart+size-1, last_offset, decode_texcb(last_texcb), decode_ap(last_ap), decode_xn(last_xn)), file=sys.stderr)
-        vstart, pstart, size = a, p, s
-        last_offset = offset
-        last_ap = ap
-        last_xn = xn
-        last_texcb = texcb
 
-    #~ print "%08X-%08X: %08X-%08X (tmp)" % (vstart, vstart+size, pstart, pstart+size)
-
-print("%08X-%08X -> %08X-%08X (%+5X) %-4s %s %s" % (vstart, vstart+size-1, pstart, pstart+size-1, last_offset, decode_texcb(last_texcb), decode_ap(last_ap), decode_xn(last_xn)), file=sys.stderr)
-
+if __name__ == "__main__":
+    main()
